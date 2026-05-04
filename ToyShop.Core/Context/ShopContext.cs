175a3ae856dfed531f679809eab0f64e87732a32
Baka <batleshemov@gmail.com>
2026-05-05 01:31:49 +0300

    Добавленная строка подключения к бд

---
+++ ToyShop.Core/Context/ShopContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyShop.Core.Models;
+
+namespace ToyShop.Core.Context
+{
+    public class ShopContext : DbContext
+    {
+
+        public DbSet<Administration> Administrations { get; set; }
+        public DbSet<Brand> brands { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Sale> sales { get; set; }
+        public DbSet<Supplier> suppliers { get; set; }
+        public DbSet<User> users { get; set; }
+
+        public ShopContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ToyShop;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30"));
+        }
+    }
+}
