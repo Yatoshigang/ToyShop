48aff17cbae1e57dd6a501c8fca1e87faca58895
Baka <batleshemov@gmail.com>
2026-05-04 17:54:05 +0300

    Добавленны статусы заказа

---
+++ ToyShop.Core/Representations/Statuses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyShop.Core.Representations
+{
+    [Flags]
+    public enum Statuses
+    {
+        Pending = 0 << 0,
+        Adopted = 0 << 1,
+        PaymentAwait = 0 << 2,
+        Paid = 0 << 3,
+        Completed = Adopted | Paid
+    }
+}
