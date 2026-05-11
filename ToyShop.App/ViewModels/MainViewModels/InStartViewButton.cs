ff8590526860a05a765508a2562cf74cf10669fd
Baka <batleshemov@gmail.com>
2026-05-11 22:51:15 +0300

    Создание структуры для кнопок, появляющихся при инициализации MainWIndow

---
+++ ToyShop.App/ViewModels/MainViewModels/InStartViewButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ToyShop.App.ViewModels.MainViewModels
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct InStartViewButton
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Content { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICommand Command { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="command"></param>
+        public InStartViewButton(string content)
+        {
+            Content = content;
+        }
+
+
+
+    }
+}
