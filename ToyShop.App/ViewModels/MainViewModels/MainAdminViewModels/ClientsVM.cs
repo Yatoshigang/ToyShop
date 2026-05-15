4d158df7edfbe1ff555575eb7a1f255369105100
Baka <batleshemov@gmail.com>
2026-05-15 05:10:26 +0300

    Создание компонента со списком клиентов

--- ToyShop.App/ViewModels/MainViewModels/MainAdminViewModels/ClientsVM.cs
+++ ToyShop.App/ViewModels/MainViewModels/MainAdminViewModels/ClientsVM.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using ToyShop.Core.Models;
 
 namespace ToyShop.App.ViewModels.MainViewModels.MainAdminViewModels
 {
-    internal class ClientsVM
+    public partial class ClientsVM : ObservableObject
     {
+        [ObservableProperty]
+        ObservableCollection<Customer> _clients;
+        public ClientsVM()
+        {
+            
+        }
+
+        void Refresh()
+        {
+            Clients = new ObservableCollection<Customer>(App.ctx.Customers.ToList());
+        }
     }
 }
