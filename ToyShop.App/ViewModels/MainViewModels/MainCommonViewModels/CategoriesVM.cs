dcc0deadd10f7430b029818e94f4ad168b58dc59
Baka <batleshemov@gmail.com>
2026-05-15 05:11:24 +0300

    Создание общих компонентов

--- ToyShop.App/ViewModels/MainViewModels/MainCommonViewModels/CategoriesVM.cs
+++ ToyShop.App/ViewModels/MainViewModels/MainCommonViewModels/CategoriesVM.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using ToyShop.Core.Models;
 
 namespace ToyShop.App.ViewModels.MainViewModels.MainCommonViewModels
 {
-    internal class CategoriesVM
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class CategoriesVM : ObservableObject
     {
+        [ObservableProperty]
+        ObservableCollection<Category> _categories;
+
+
+        public CategoriesVM()
+        {
+            Categories = new ObservableCollection<Category>(App.ctx.Categories.ToList());
+        }
+
+        [RelayCommand]
+        void GoToProducts(string category)
+        {
+            (App.Current.MainWindow.DataContext as MainWindowVM)?.GoToProductsComponentWithCondition(category);
+        }
     }
 }
